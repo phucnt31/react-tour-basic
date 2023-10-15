b49067f540a87407b3c6a4b973985af84c64e87a
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 21:08:09 +0700

    Add read more function

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ image, info, name, price, id, removeItem }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <article className="single-tour">
       <span className="tour-price">${price}</span>
       <img src={image} alt={name} className="img" />
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {showInfo ? info : info.slice(0, 200) + "... "}
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => setShowInfo(!showInfo)}
+          >
+            {showInfo ? "Show less" : "Read more"}
+          </button>
+        </p>
         <button
           type="button"
           className="delete-btn btn-block btn"
