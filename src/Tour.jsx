71a17e08c7ad7d7bd8ed89851c3fdca86dac7193
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 20:57:27 +0700

    Render individual tour

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Tour = ({ image, info, name, price }) => {
-  return <div>Tour</div>;
+  return (
+    <div className="single-tour">
+      <div className="tour-price">${price}</div>
+      <img src={image} alt={name} className="img" />
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>{info}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Tour;
