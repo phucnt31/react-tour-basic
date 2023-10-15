0e6647cca587f45e4ddaf40ef45dce6bd8a64f44
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 21:35:42 +0700

    Adjust css for title

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -3,14 +3,16 @@ import Tour from "./Tour";
 
 const Tours = ({ tours, removeItem }) => {
   return (
-    <section className="tours">
+    <section>
       <div className="title">
         <h3>Our torus</h3>
         <div className="title-underline"></div>
       </div>
-      {tours.map((tour) => {
-        return <Tour key={tour.id} {...tour} removeItem={removeItem} />;
-      })}
+      <div className="tours">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeItem={removeItem} />;
+        })}
+      </div>
     </section>
   );
 };
