a3601c1e0afe2103b44d5e4ca9587ca3b4f099f9
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 20:49:02 +0700

    Fetch tours from outside API and customize Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tours from "./Tours";
+import Loading from "./Loading";
 
 const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
   const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchTours = async () => {
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setIsLoading(false);
+      setTours(data);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 
   return (
     <main>
