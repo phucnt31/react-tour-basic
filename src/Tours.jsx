8bdaafb57b55789e673289d0e787b63e11b3f007
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-15 21:04:11 +0700

    remove console.log

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Tour from "./Tour";
 
 const Tours = ({ tours, removeItem }) => {
-  console.log(tours);
   return (
     <section className="tours">
       {tours.map((tour) => {
